34117874bdd2daaf9d16872a81e849618af240d6
Jeremy Park <seungilpark88@gmail.com>
2020-05-27 14:50:52 -0700

    different output scheme, automatic tagging feature start

--- fetchKidsActive.js
+++ fetchKidsActive.js
@@ -1,6 +1,5 @@
 /* 
-    a script to fetch events from activeacess.com
-    const url = http://api.amp.active.com/v2/search?kids=true&category=event&start_date=2020-05-26..&near=Vancouver,BC,CA&radius=200&per_page=100&current_page=0&api_key=pyjj9te7dvz59wdf2ag3m9ms
+    a script to fetch events from activeacess.com, to map them into tmkd objects, to insert into tmkd db, and to write the result into json
 */
 
 const fs = require("fs");
@@ -11,7 +10,6 @@ require("dotenv").config();
 const axios = require("axios");
 const API_KEY = process.env.ACTIVE_DOT_COM_API_KEY;
 const OUTPUT_DIR = "./KidsActiveResult";
-const OUTPUT_FILENAME ="/KidsActiveEventsResult"
 const mysql = require("mysql");
 
 let dbConfig = {
@@ -33,6 +31,27 @@ const pool = mysql.createPool(dbConfig);
 // Detail Query Example
 // GET by id
 // http://api.amp.active.com/v2/search?place_id="PLACE_ID"&api_key=pyjj9te7dvz59wdf2ag3m9ms
+// event object from the api
+// https://developer.active.com/io-docs
+
+// type TMKD event {
+//   vendor_id : int,
+//   name: string,
+//   event_type: string,
+//   start_date: datetime,
+//   end_date: datetime,
+//   link: string,
+//   address:
+//   city:
+//   province:
+//   lat:
+//   lng:
+//   description
+//   image_url
+//   source_id
+//   source_platform
+//   source_endpoint
+// }
 
 
 // configure params here
@@ -59,120 +78,119 @@ const CITIES = [
 ]
  
 const RESULT_ITEMS = []
+const RESULT_ITEMS_TAG_INFO = []
 const RESULT_DICT = {}
-// type TMKD event {
-//   vendor_id : int,
-//   name: string,
-//   event_type: string,
-//   start_date: datetime,
-//   end_date: datetime,
-//   link: string,
-//   address:
-//   city:
-//   province:
-//   lat:
-//   lng:
-//   description
-//   image_url
-//   source_id
-//   source_platform
-//   source_endpoint
-// }
+
         
+/**
+ * @param  {} kidsActivityEventObject
+ * input: event fetched from the api
+ * output tmkd event  
+ */
 function mapper(kidsActivityEventObject) {
   try {
-
     const tmkd_event = {}
     tmkd_event.vendor_id = VENDOR_ID
     tmkd_event.name = kidsActivityEventObject.assetName
     tmkd_event.event_type = kidsActivityEventObject.place.placeName === "ONLINE" ? "virtual" : "activity"
     tmkd_event.start_date = kidsActivityEventObject.salesStartDate
     tmkd_event.end_date = kidsActivityEventObject.salesEndDate
+    tmkd_event.start_time = kidsActivityEventObject.activityRecurrences[0].startTime
+    tmkd_event.end_time = kidsActivityEventObject.activityRecurrences[0].endtime
     tmkd_event.link = kidsActivityEventObject.registrationUrlAdr
     tmkd_event.address = kidsActivityEventObject.place.addressLine1Txt
     tmkd_event.city = kidsActivityEventObject.place.cityName
     tmkd_event.province = kidsActivityEventObject.place.stateProvinceCode
     tmkd_event.lat = kidsActivityEventObject.place.latitude
     tmkd_event.lng = kidsActivityEventObject.place.longitude
+    
     tmkd_event.description = (kidsActivityEventObject.assetDescriptions&& kidsActivityEventObject.assetDescriptions[0].description) || "no description found"
+    // tmkd_event.description = (kidsActivityEventObject.organization && kidsActivityEventObject.organization.organizationDsc) || "no description found"
+    // console.log("assetDesc:", kidsActivityEventObject.assetDescriptions && kidsActivityEventObject.assetDescriptions[0].description)
+    // console.log("organizationDsc:", kidsActivityEventObject.organization && kidsActivityEventObject.organization.organizationDsc)
     tmkd_event.image_url = kidsActivityEventObject.logoUrlAdr
     tmkd_event.source_id = kidsActivityEventObject.assetGuid
     tmkd_event.source_platform = "active.com"
     tmkd_event.source_endpoint = `http://api.amp.active.com/v2/search?place_id=${kidsActivityEventObject.assetGuid}`
+    
+    // TODO: do something with the tags
+    const tags = kidsActivityEventObject.assetTags.map(t => t.tag && ({key: t.tag.tagDescription, value: t.tag.tagName }))
+    const categories = kidsActivityEventObject.assetCategories.map(c => c.category && c.category.categoryName)
+    const topics = kidsActivityEventObject.assetTopics.map(t => t.topic && ({ topicTaxonomy: t.topic.topicTaxonomy, topicName:  t.topic.topicName}))
+    console.log ( { id: tmkd_event.source_id, tags, categories, topics, idInDb: null } )
+    
+    RESULT_DICT[tmkd_event.source_id] = 1
+    RESULT_ITEMS_TAG_INFO.push({ id: tmkd_event.source_id, tags, categories, topics, idInDb: null })
     return tmkd_event
 
   } catch(err) {
-    console.error("item of id = ", kidsActivityEventObject.assetGuid)
+    console.error("Error in mapper()")
+    console.error("assetGuid = ", kidsActivityEventObject.assetGuid) 
+    
     return null
     
   }  
-
-  
 }
-
-
 
 
 /**
  * @param  { string } cityName
  * @param  { int } pageNum
  * fetch kidsactivity_events []
- * return tmkd_events[] || []
+ * update RESULT_ITEMS, RESULT_ITEMS_TAG_INFO global variables
  */
 async function fetchEvents(cityName) {
-  // const output = []
   
-  let pageNum = 0;
-  let url = `http://api.amp.active.com/v2/search?kids=true&category=event&start_date=${STR_TODAY}..&near=${cityName},BC,CA&radius=40&per_page=100&current_page=${pageNum}&api_key=${API_KEY}`
-  console.log({ url });
+  let pageNum = 1;
+  let url = `http://api.amp.active.com/v2/search?kids=true&category=event&start_date=${STR_TODAY}..&near=${cityName},BC,CA&radius=${RADIUS}&per_page=100&current_page=${pageNum}&api_key=${API_KEY}`
+  // console.log({ url });
+  console.log(`fetching events for ${cityName}...`, "\nRESULT_ITEMS.length", RESULT_ITEMS.length)
   let response = await axios.get(url);
   let { results } = response.data
-  
   // console.log({results})
   
+  
   while (results.length) {
-    
+    console.log(`${results.length} events fetched!`)  
     for (let event of results) {
       // console.log(`fetched event: ${JSON.stringify(event, undefined, 4)}`)
 
-      if (event.assetRegistrableStatus !== "reg-open" || event.localeCd !== "en_CA" || event.salesStatus !== "registration-open") {
+      if (event.assetRegistrableStatus !== "reg-open" || event.place.countryCode !== "CAN" || event.salesStatus !== "registration-open") {
+        // console.log({ status: event.assetRegistrableStatus, localeCd: event.localeCd, countryCode: event.place.countryCode, salesStatus: event.salesStatus  })
         continue
       } // check invalid event                                    
 
       if (RESULT_DICT[event.assetGuid]) {
         continue
-      } else {
-        RESULT_DICT[event.assetGuid] = 1
-      }
-      //check duplicated                    
+      } //check duplicated                    
 
       const tmkdEvent = mapper(event)
-      // output.push(tmkdEvent)
       if (tmkdEvent) RESULT_ITEMS.push(tmkdEvent)
-      // else continue
 
     }
-    // await new Promise((resolve, reject) => {
-    //   setTimeout(resolve.bind(null, undefined), 2500);
-    // }); // some padding
+
     await _Sleep(2500)
+    
     try {
       pageNum++
       url = `http://api.amp.active.com/v2/search?kids=true&category=event&start_date=${STR_TODAY}..&near=${cityName},BC,CA&radius=40&per_page=100&current_page=${pageNum}&api_key=${API_KEY}`
+      console.log(`fetching events for ${cityName}...`, "\nRESULT_ITEMS.length", RESULT_ITEMS.length)
       response = await axios.get(url);
       results = response.data.results
-      console.log({pageNum, url})
+      // console.log({pageNum, url})
 
     } catch(err) {
       console.error(err)
       continue
     }
   } 
-
-  // return output
-
 }
 
+
+/**
+ * @param  {} ms
+ * used to give interval between requests
+ */
 function _Sleep(ms) {
  return new Promise((resolve, reject) => {
   setTimeout(resolve.bind(null, undefined), ms);
@@ -186,18 +204,18 @@ async function main() {
   // filter duplicated event using assetIGuid field
   // enter event to items []
   // write items as result.json
+    console.time("FETCHING PROCESS");
     for (let cityName of CITIES) {
       await fetchEvents(cityName)
-      console.log(`fetching events for ${cityName}...`, "\nRESULT_ITEMS.length", RESULT_ITEMS.length)
       await _Sleep(2500) // some padding
     }
+    console.timeEnd("FETCHING PROCESS");
     // console.log({ RESULT_ITEMS })
     
     
     // loop through tmkdEventObjects and insert each one to db
-    const sql = `
-        INSERT INTO event_test3 SET ?
-    `
+    console.log("START INSERTING TO DB...")
+    const sql = `INSERT INTO event_test3 SET ?`
     for (let tmkdEvent of RESULT_ITEMS) {
       try {
         const insertResult = await new Promise((resolve, reject) => {
@@ -206,7 +224,9 @@ async function main() {
             else resolve(rows)
           })
         }) 
-        console.log({"inserted id": insertResult.insertId})
+        
+        RESULT_ITEMS_TAG_INFO.find( item => item.id === tmkdEvent.source_id ).idInDb = insertResult.insertId
+        // console.log({"inserted id": insertResult.insertId})
         
       } catch (error) {
         console.error("error while inserting to db,...")
@@ -215,17 +235,27 @@ async function main() {
       }
 
     }
-    
+    console.log("FINISHED INSERTING TO DB...")
     // write the array of tmkd objects into json
-    
+    console.log(`WRITING JSONS`)
     if (!fs.existsSync(OUTPUT_DIR)){
       fs.mkdirSync(OUTPUT_DIR);
     }
-
-    fs.writeFileSync(OUTPUT_DIR +`${OUTPUT_FILENAME}_${STR_TODAY}.json`, JSON.stringify(RESULT_ITEMS, undefined, 4))
+    
+    if (!fs.existsSync(OUTPUT_DIR + '/' + STR_TODAY)){
+      fs.mkdirSync(OUTPUT_DIR + '/' + STR_TODAY);
+    }
+    
+    fs.writeFileSync(OUTPUT_DIR + '/' + STR_TODAY + '/' + "RESULT_ITEMS.json", JSON.stringify(RESULT_ITEMS, undefined, 4))
+    fs.writeFileSync(OUTPUT_DIR + '/' + STR_TODAY + '/' + "RESULT_ITEMS_TAG_INFO.json", JSON.stringify(RESULT_ITEMS_TAG_INFO, undefined, 4))
+    
+    console.log(`FINISHED WRITING JSONS`)
+    console.log("JOB FINISHED...")
+    process.exit(0);
 
   } catch (error) {
     console.error(error.message);
+    process.exit(1);
   }
 }
 
